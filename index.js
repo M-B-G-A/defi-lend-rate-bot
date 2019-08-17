b91bdcab970b4afeca707f61b753321eaca3e17d
Jun Park <pj2417@gmail.com>
2019-08-18 00:53:44 +0900

    poloniex and bitfinex implemented

--- index.js
+++ index.js
@@ -56,6 +56,64 @@ async function defiPulse(token) {
     return resString;
 }
 
+async function bitfinex() {
+    const usdRes = await axios.get("https://api.bitfinex.com/v1/lends/usd");
+    const usdtRes = await axios.get("https://api.bitfinex.com/v1/lends/ust");
+
+    let usdSum = 0;
+    let usdtSum = 0;
+
+    for(let i = 0; i < 50; i++) {
+        usdSum += usdRes.data[i].rate * 1;
+        usdtSum += usdtRes.data[i].rate * 1;
+    }
+    const usdAvg = Math.round(usdSum * 2) / 100;
+    const usdtAvg = Math.round(usdtSum * 2) / 100;
+
+    let resString = "Bitfinex\n=====================\n";
+    resString += "USD lend rate: " + usdAvg + "%\n";
+    resString += "USDT lend rate: " + usdtAvg + "%\n\n";
+    resString += "average lend data of recent 50"
+
+    return resString;
+}
+
+async function poloniex() {
+    const usdtRes = await axios.get("https://poloniex.com/public?command=returnLoanOrders&currency=USDT");
+    const usdcRes = await axios.get("https://poloniex.com/public?command=returnLoanOrders&currency=USDC");
+
+    const usdtOffer = usdtRes.data.offers;
+    const usdcOffer = usdcRes.data.offers;
+
+    let usdtSum = 0;
+    let usdtRateSum = 0;
+    let i = 0;
+    while(usdtSum < 10000) {
+        usdtSum += usdtOffer[i].amount * 1;
+        usdtRateSum += (usdtOffer[i].rate * 365) * (usdtOffer[i].amount * 1);
+        i++;
+    }
+
+    let usdcSum = 0;
+    let usdcRateSum = 0;
+    i = 0;
+    while(usdcSum < 20000) {
+        usdcSum += usdcOffer[i].amount * 1;
+        usdcRateSum += (usdcOffer[i].rate * 365) * (usdcOffer[i].amount * 1);
+        i++;
+    }
+    
+    const usdtAvg = Math.round(usdtRateSum / usdtSum * 10000) / 100;
+    const usdcAvg = Math.round(usdcRateSum / usdcSum * 10000) / 100;
+
+    let resString = "Poloniex\n=====================\n";
+    resString += "USDT lend rate: " + usdtAvg + "%\n";
+    resString += "USDC lend rate: " + usdcAvg + "%\n\n";
+    resString += "avg rate to lend usdt $10000, usdc $20000"
+
+    return resString;
+}
+
 async function main() {
     const bot = new TelegramBot(token);
 
@@ -63,6 +121,10 @@ async function main() {
     bot.sendMessage("@defi_lend_rate", daiRes);
     const usdcRes = await defiPulse("USDC");
     bot.sendMessage("@defi_lend_rate", usdcRes);
+    const poloniexRes = await poloniex();
+    bot.sendMessage("@defi_lend_rate", poloniexRes);
+    const bitfinexRes = await bitfinex();
+    bot.sendMessage("@defi_lend_rate", bitfinexRes);
     return;
 }
 
